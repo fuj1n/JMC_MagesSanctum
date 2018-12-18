2a247c35e792e19cdd3cfafed069a65eb5d22010
Arthur Uzulin <fuj1nman@hotmail.com>
2018-12-18 17:38:29 +1100

    Made arm glow oscillate and updated tile materials

---
+++ MagesSanctum/Assets/Scripts/EmissiveOscillator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EmissiveOscillator : MonoBehaviour
+{
+    public float frequency = 1F;
+    public float amplitude = 2F;
+    public float offset = .5F;
+
+    [ColorUsage(false)]
+    public Color min;
+    [ColorUsage(false)]
+    public Color max;
+
+    public Renderer render;
+
+    private float time;
+
+    private void Update()
+    {
+        float val = offset + Mathf.Sin(time) * amplitude;
+
+        render.material.SetColor("_EmissionColor", Color.Lerp(min, max, val));
+
+        time += Time.deltaTime * frequency;
+
+        while (time > 2 * Mathf.PI)
+            time -= 2 * Mathf.PI;
+    }
+}
